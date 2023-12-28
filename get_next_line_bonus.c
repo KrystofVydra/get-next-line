d659b9df5ded0dc0747bef85538854dea2841ba7
Kryštof <krystofvydra2016@seznam.cz>
2023-12-28 11:05:16 +0100

    added bonus

---
+++ get_next_line_bonus.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kvydra <kvydra@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/14 16:55:27 by kvydra            #+#    #+#             */
+/*   Updated: 2023/12/28 10:58:43 by kvydra           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <stdlib.h>
+#include <unistd.h>
+
+char	*get_next_line(int fd)
+{
+	char			*output;
+	unsigned int	total_size;
+	unsigned int	index;
+	int				readrtval;
+
+	output = NULL;
+	total_size = 0;
+	index = 0;
+	readrtval = 0;
+	output = main_reading(readrtval, index, &total_size, fd);
+	if (!output)
+		return (NULL);
+	output[total_size] = '\0';
+	return (output);
+}
--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kvydra <kvydra@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/14 16:56:03 by kvydra            #+#    #+#             */
-/*   Updated: 2023/12/28 11:00:01 by kvydra           ###   ########.fr       */
+/*   Updated: 2023/12/28 11:00:03 by kvydra           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 1
